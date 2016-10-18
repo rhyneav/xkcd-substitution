01c8bf7c9ace311339d07382396eb3478b8d5092
Rhyne <rhyneav@gmail.com>
2016-10-18 20:59:58 +0000

    add substitutions 2 and 3

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function matchCase(text, pattern) {
-    var result = '';
+    var result = "";
 
     for(var i = 0; i < text.length; i++) {
         var c = text.charAt(i);
@@ -22,7 +22,7 @@ function htmlreplace(a, b, element) {
     
     for (var n=0; n<nodes.length; n++) {
         if (nodes[n].nodeType == Node.TEXT_NODE) {
-            var r = new RegExp(a, 'gi');
+            var r = new RegExp(a, "gi");
             nodes[n].textContent = nodes[n].textContent.replace(r, function(match) {
                 return matchCase(b, match);
             });
@@ -34,20 +34,59 @@ function htmlreplace(a, b, element) {
 
 // Object of substitutions
 var xkcd = {
-    'witnesses': 'these dudes I know',
-    'allegedly': 'kinda probably',
-    'new study': 'Tumblr post',
-    'rebuild': 'avenge',
-    'space': 'spaaace',
-    'google glass': 'virtual boy',
-    'smartphone': 'pokedex',
-    'electric': 'atomic',
-    'senator': 'elf-lord',
-    'car': 'cat',
-    'election': 'eating contest',
-    'congressional leader': 'river spirit',
-    'homeland security': 'homestar runner',
-    'could not be reached for comment': 'is guilty and everyone knows it'
+    "witnesses": "these dudes I know",
+    "allegedly": "kinda probably",
+    "new study": "Tumblr post",
+    "rebuild": "avenge",
+    "space": "spaaace",
+    "google glass": "virtual boy",
+    "smartphone": "pokedex",
+    "electric": "atomic",
+    "senator": "elf-lord",
+    "car": "cat",
+    "election": "eating contest",
+    "congressional leader": "river spirit",
+    "homeland security": "homestar runner",
+    "could not be reached for comment": "is guilty and everyone knows it",
+    "debate": "dance-off",
+    "self driving": "uncontrollably swerving",
+    "poll": "psychic reading",
+    "candidate": "airbender",
+    "drone": "dog",
+    "vows to": "probably won't",
+    "at large": "very large",
+    "second-degree": "friggin awful",
+    "third-degree": "friggin awful",
+    "an unknown number": "like hundreds",
+    "front runner": "blade runner",
+    "global": "spherical",
+    "years": "minutes",
+    "minutes": "years",
+    "no indication": "lots of signs",
+    "urged restraint by": "drunkenly egged on",
+    "horsepower": "tons of horsemeat",
+    "gaffe": "magic spell",
+    "ancient": "haunted",
+    "star-studded": "blood-soaked",
+    "remains to be seen": "will never be known",
+    "silver bullet": "ways to kill werewolves",
+    "subway system": "tunnels I found",
+    "surprising": "surprising (but not to me)",
+    "war of words": "interplanetary war",
+    "tension": "sexual tension",
+    "cautiously optimistic": "delusional",
+    "doctor who": "the big bang theory",
+    "win votes": "find pokemon",
+    "behind the headlines": "beyond the grave",
+    "email": "poem",
+    "facebook post": "poem",
+    "tweet": "poem",
+    "facebook ceo": "this guy",
+    "latest": "final",
+    "disrupt": "destroy",
+    "meeting": "menage a trois",
+    "scientists": "channing tatum and his friends",
+    "you won't believe": "I'm really sad about"
 };
 
 // Script that does the actual find/replace on page
